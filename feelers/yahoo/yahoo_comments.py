bf68924274e9efbbf45e94c8d51d16502f05f045
Andrew Han <handrew11@gmail.com>
2016-08-27 14:34:16 -0700

    writing to database

--- feelers/yahoo/yahoo_comments.py
+++ feelers/yahoo/yahoo_comments.py
@@ -3,6 +3,8 @@ import json
 from bs4 import BeautifulSoup
 import re
 import pdb
+import psycopg2 
+from db_settings import *
 
 YAHOO_NEWS_URLS = ["https://www.yahoo.com/news/"]
 YAHOO_COMMENT_SERVICE_URL = 'https://www.yahoo.com/news/_td/api/resource/CommentsService.comments;count=100;publisher=news-en-US;sortBy=highestRated;uuid=%s?bkt=Headline-Testing-Control&dev_info=0&device=desktop&intl=us&lang=en-US&site=fp&returnMeta=true'
@@ -10,16 +12,52 @@ YAHOO_COMMENT_SERVICE_URL = 'https://www.yahoo.com/news/_td/api/resource/Comment
 def main():
 	uuids = get_uuids(YAHOO_NEWS_URLS[0])
 	comments = get_yahoo_comments(uuids)
-	pdb.set_trace()
+	upload_comments_to_database(comments)
+
+# take whatever is returned from 
+# get_yahoo_comments(uuids) and format for database upload
+# then upload to the database
+def upload_comments_to_database(comments):
+	# prepare comments for database 
+	comments_for_database_upload = []
+	for uuid in comments:
+		article_comments_data = comments[uuid]['comments']['data']
+		if "list" in article_comments_data:
+			for comment_json in article_comments_data['list']:
+				comment_for_upload = {}
+				comment_for_upload['yahoo_uuid'] = uuid
+				comment_for_upload['article_headline'] = comments[uuid]['article_headline']
+				comment_for_upload['article_url'] = comments[uuid]['article_url']
+				comment_for_upload['comment_text'] = comment_json['content']
+
+				comment_for_upload['reply_count'] = comment_json['replyCount']
+				comment_for_upload['thumbs_up_count'] = comment_json['thumbsUpCount']
+				comment_for_upload['thumbs_down_count'] = comment_json['thumbsDownCount']
+				comment_for_upload['author'] = comment_json['creator']
+				comment_for_upload['created_on'] = comment_json['createTime']
+
+				comments_for_database_upload.append(comment_for_upload)
+
+	# need to do the "insert if" logic
+	try:
+	    conn = psycopg2.connect("dbname='%s' user='%s' host='%s' port='%d' password='%s'" \
+	     % (DB_DATABASE, DB_USERNAME, DB_ENDPOINT, DB_PORT, DB_PASSWORD))
+	except:
+	    print "### PROBLEMS CONNECTING TO DATABASE ###"
+
 
 # GET COMMENTS FOR ARTICLES
 # take in a dictionary of {uuid => {title, url}}
-# return dictionary of {uuid => {"comments": comment_json}}
+# return dictionary of {uuid => {"article_headline": headline, "url": url, "comments": comment_json}}
 def get_yahoo_comments(uuids):
 	comments = {}
 	for uuid in uuids:
 		comment_json = get_yahoo_comments_for_article(uuid)
-		comments[uuid] = {"article_headline": uuids[uuid]['title'], "comments": comment_json}
+		comments[uuid] = {
+			"article_headline": uuids[uuid]['title'], 
+			"article_url": uuids[uuid]['url'],
+			"comments": comment_json
+		}
 		
 	return comments
 
@@ -37,7 +75,6 @@ def get_uuids(yahoo_link):
 		stream_items = stream_items[stream_items.keys()[0]]
 		stream_items = stream_items['data']['stream_items']
 		return stream_items
-
 
 	pattern = '({"context":.+});'
 	r = requests.get(yahoo_link)
